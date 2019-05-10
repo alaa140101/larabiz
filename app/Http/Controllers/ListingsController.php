3cb46cd4162569712a880deb0136e40f0b26b238
alaa140101 <alaa1400@hotmail.com>
2019-05-10 09:23:11 +0300

    Access control

--- app/Http/Controllers/ListingsController.php
+++ app/Http/Controllers/ListingsController.php
@@ -12,9 +12,17 @@ class ListingsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+
+    public function __construct(){
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+
     public function index()
     {
-        //
+        $listings = Listing::orderby('created_at', 'desc')->get();
+        return view('listings')->with('listings', $listings);
+
     }
 
     /**
@@ -63,7 +71,8 @@ class ListingsController extends Controller
      */
     public function show($id)
     {
-        //
+        $listing = Listing::find($id);
+        return view('showlisting')->with('listing', $listing);
     }
 
     /**
